5d8a53d26bfc7ad33262311ecb98cceb1a7026a6
kay campbell <kayzanthia@gmail.com>
2017-05-08 21:57:02 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,180 @@
+
+/* Set the width of the side navigation to 250px */
+/*
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+/*
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+*/
+/* Open the sidenav */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+}
+
+/* Close/hide the sidenav */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+/*--SCROLL FUNCTION--*/
+/*
+$('.js--scroll-to-contacts').click(function (){
+        $('html, body').animate({scrollTop: $('.js--section-contacts').offset().top}, 1000);
+    });
+    
+    
+    
+    
+    
+   $('.js--scroll-to-features').click(function (){
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
+    });
+
+
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+*/
+/*animations-fade in etc*/
+/*
+    $('.js--wp-1').waypoint(function(direction) {
+      $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset:'50%'
+    });
+    
+    $('.js--wp-2').waypoint(function(direction) {
+      $('.js--wp-2').addClass('animated fadeInUp');
+    }, {
+        offset:'50%'
+    });
+    
+    $('.js--wp-3').waypoint(function(direction) {
+      $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+        offset:'50%'
+    });
+    
+    $('.js--wp-4').waypoint(function(direction) {
+      $('.js--wp-4').addClass('animated pulse');
+    }, {
+        offset:'50%'
+    });
+    
+*/
+
+
+/*--SCROLL TECH */
+/*
+    window.sr = ScrollReveal();
+        sr.reveal('.navbar',{
+            
+            duration: 2000,
+            origin: 'bottom'
+        });
+     sr.reveal('.showcase-left',{
+            
+            duration: 2000,
+            origin: 'top',
+            distance: '300px'
+        });
+    sr.reveal('.showcase-right',{
+            
+            duration: 2000,
+            origin: 'right',
+            distance: '300px'
+        });
+        sr.reveal('.showcase-btn',{
+            
+            duration: 2000,
+            delay: 2000,
+            origin: 'bottom',
+        });
+        sr.reveal('#testimonial div',{
+            
+            duration: 2000,
+            origin: 'bottom',
+        });
+        
+        sr.reveal('.info-left',{
+            
+            duration: 2000,
+            origin: 'left',
+            distance: '300px',
+            viewFactor: 0.2
+        });
+        sr.reveal('.info-right',{
+            
+            duration: 2000,
+            origin: 'right',
+            distance: '300px',
+            viewFactor: 0.2
+        });
+        
+    </script>
+    <script>
+     $(function() {
+      // Smooth Scrolling
+      $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
+          }
+        }
+      });
+    });
+    
+    */
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
